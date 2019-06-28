f76e005fc23c0f2a3a72d8151a56216a4f825809
周鑫 <15901263220@163.com>
2019-06-28 14:13:48 +0800

    初始化

---
+++ ToolBox/ToolBox/Resource/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToolBox
+//
+//  Created by 周鑫 on 2019/6/27.
+//  Copyright © 2019 ZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ToolBox/ToolBox/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ToolBox
+//
+//  Created by 周鑫 on 2019/6/27.
+//  Copyright © 2019 ZX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
