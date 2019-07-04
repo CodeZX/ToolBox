9a260b2ab43eac22f4fa4497cd05349b2521d3a8
周鑫 <15901263220@163.com>
2019-07-04 17:48:29 +0800

    loverCode 控件和Model 关联

--- ToolBox/ToolBox/ToolBox/ViewModel/TBLoverCodeViewModel.m
+++ ToolBox/ToolBox/ToolBox/ViewModel/TBLoverCodeViewModel.m
@@ -11,7 +11,8 @@
 #import "TBBriefIntroductionView.h"
 #import "TBSexSelectButton.h"
 #import "TBBirthdaySelectButton.h"
-@interface TBLoverCodeViewModel ()
+#import "TBLoverCodeModel.h"
+@interface TBLoverCodeViewModel ()<UITextFieldDelegate>
 
 @property (nonatomic,strong) UIViewController  *target;
 @property (nonatomic,weak) TBBriefIntroductionView *briefIntroductionView;
@@ -22,10 +23,13 @@
 @property (nonatomic,weak) TBSexSelectButton *womanSexSelectButton;
 @property (nonatomic,weak) UILabel *oneSelfbBirthdaySelectLabel;
 @property (nonatomic,weak) UILabel *fereBirthdaySelectLabel;
-@property (nonatomic,weak) TBBirthdaySelectButton *oneSelfBirthdaySelectButton;
-@property (nonatomic,weak) TBBirthdaySelectButton *fereBirthdaySelectButton;
+@property (nonatomic,weak) UITextField *oneSelfBirthdaySelectTextField;
+@property (nonatomic,weak) UITextField *fereBirthdaySelectTextField;
 @property (nonatomic,weak) UIButton *matchingButton;
 @property (nonatomic,weak) UILabel *promptLabel;
+
+@property (nonatomic,strong) TBLoverCodeModel *loverCodeModel;
+@property (nonatomic,strong) UIDatePicker *datePicker;
 
 @end
 
@@ -40,6 +44,7 @@ static NSString * const identifier = @"loverCode";
         [self setupTarget:target];
         [self setupComponent];
         [self setupSourceData];
+        [self setupAssociate];
         
     }
     return self;
@@ -96,12 +101,14 @@ static NSString * const identifier = @"loverCode";
     }];
     
     TBSexSelectButton *manSexSelectButton = [[TBSexSelectButton alloc]init];
+    manSexSelectButton.backgroundColor = [UIColor redColor];
     [manSexSelectButton setTitle:@"男" forState:UIControlStateNormal];
     [manSexSelectButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [manSexSelectButton addTarget:self action:@selector(sexButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
     [self.target.view addSubview:manSexSelectButton];
     self.manSexSelectButton = manSexSelectButton;
     [self.manSexSelectButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.sexSelectLabel);
+        make.centerY.equalTo(self.sexSelectLabel);
         make.left.equalTo(self.sexSelectLabel.right).offset(10);
         
     }];
@@ -109,10 +116,12 @@ static NSString * const identifier = @"loverCode";
     TBSexSelectButton *womanSexSelectButton = [[TBSexSelectButton alloc]init];
     [womanSexSelectButton setTitle:@"女" forState:UIControlStateNormal];
     [womanSexSelectButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [womanSexSelectButton addTarget:self action:@selector(sexButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+
     [self.target.view addSubview:womanSexSelectButton];
     self.womanSexSelectButton = womanSexSelectButton;
     [self.womanSexSelectButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.manSexSelectButton);
+        make.centerY.equalTo(self.manSexSelectButton);
         make.left.equalTo(self.manSexSelectButton.right).offset(10);
     }];
     
@@ -127,13 +136,14 @@ static NSString * const identifier = @"loverCode";
     }];
     
     
-    TBBirthdaySelectButton *oneSelfBirthdaySelectButton = [[TBBirthdaySelectButton alloc]init];
-    [oneSelfBirthdaySelectButton setTitle:@"请选择您的生日" forState:UIControlStateNormal];
-    [oneSelfBirthdaySelectButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [self.target.view addSubview:oneSelfBirthdaySelectButton];
-    self.oneSelfBirthdaySelectButton = oneSelfBirthdaySelectButton;
-    [self.oneSelfBirthdaySelectButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.oneSelfbBirthdaySelectLabel);
+    UITextField *oneSelfBirthdaySelectTextField = [[UITextField alloc]init];
+    oneSelfBirthdaySelectTextField.placeholder = @"请选择你的生日";
+    oneSelfBirthdaySelectTextField.inputView = self.datePicker;
+    oneSelfBirthdaySelectTextField.delegate = self;
+    [self.target.view addSubview:oneSelfBirthdaySelectTextField];
+    self.oneSelfBirthdaySelectTextField = oneSelfBirthdaySelectTextField;
+    [self.oneSelfBirthdaySelectTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.oneSelfbBirthdaySelectLabel);
         make.left.equalTo(self.oneSelfbBirthdaySelectLabel.right).offset(10);
     }];
     
@@ -150,13 +160,16 @@ static NSString * const identifier = @"loverCode";
     }];
     
     
-    TBBirthdaySelectButton *fereBirthdaySelectButton = [[TBBirthdaySelectButton alloc]init];
-    [fereBirthdaySelectButton setTitle:@"请选择恋人生日" forState:UIControlStateNormal];
-    [fereBirthdaySelectButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [self.target.view addSubview:fereBirthdaySelectButton];
-    self.fereBirthdaySelectButton = fereBirthdaySelectButton;
-    [self.fereBirthdaySelectButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.fereBirthdaySelectLabel);
+    
+  
+    UITextField *fereBirthdaySelectTextField = [[UITextField alloc]init];
+    fereBirthdaySelectTextField.placeholder = @"请选择恋人生日";
+    fereBirthdaySelectTextField.inputView = self.datePicker;
+    fereBirthdaySelectTextField.delegate = self;
+    [self.target.view addSubview:fereBirthdaySelectTextField];
+    self.fereBirthdaySelectTextField = fereBirthdaySelectTextField;
+    [self.fereBirthdaySelectTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.fereBirthdaySelectLabel);
         make.left.equalTo(self.fereBirthdaySelectLabel.right).offset(10);
     }];
     
@@ -183,14 +196,101 @@ static NSString * const identifier = @"loverCode";
 }
 
 - (void)setupSourceData {
+
+    self.loverCodeModel = [[ TBLoverCodeModel alloc]init];
+    
+}
+
+- (void)setupAssociate {
+
+   
+    [self.manSexSelectButton addObserver:self forKeyPath:@"selected" options:NSKeyValueObservingOptionNew context:nil];
+    [self.womanSexSelectButton addObserver:self forKeyPath:@"selected" options:NSKeyValueObservingOptionNew context:nil];
+    [self.oneSelfBirthdaySelectTextField addObserver:self forKeyPath:@"text" options:NSKeyValueObservingOptionNew context:nil];
+    [self.fereBirthdaySelectTextField addObserver:self forKeyPath:@"text" options:NSKeyValueObservingOptionNew context:nil];
+   
+   
     
     
 }
 
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    
+    if ([keyPath isEqualToString:@"selected"]) {
+        
+        NSLog(@"zxzxzxzc");
+        
+    }
+    if ([keyPath isEqualToString:@"text"]) {
+        
+       
+        NSLog(@"%@",change[@"kind"]);
+        NSLog(@"%@",change[@"new"]);
+    }
+}
 
 - (NSString *)stringForIdentifier {
     return identifier;
 }
 
+
+#pragma mark ------------------------------------ Means -------------------------------------------
+#pragma mark private
+
+#pragma mark public
+#pragma mark respond
+- (void)sexButtonClicked:(TBSexSelectButton *)sexSelectButton {
+    
+    sexSelectButton.selected = !sexSelectButton;
+    
+}
+
+
+#pragma mark -----------------------------------Delegagte ------------------------------------------
+//禁止用户输入文字
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    return NO;
+}
+
+- (void)dateChange:(UIDatePicker *)datePicker {
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+    //设置时间格式
+    formatter.dateFormat = @"yyyy/MM/dd";
+    NSString *dateStr = [formatter  stringFromDate:datePicker.date];
+    
+    self.oneSelfBirthdaySelectTextField.text = dateStr;
+}
+#pragma mark ----------------------------------- Network -------------------------------------------
+#pragma mark --------------------------------- Lazy loading ----------------------------------------
+
+- (UIDatePicker *)datePicker {
+    if (!_datePicker) {
+        _datePicker = [[UIDatePicker alloc] init];
+        
+        //设置地区: zh-中国
+        _datePicker.locale = [NSLocale localeWithLocaleIdentifier:@"zh"];
+        
+        //设置日期模式(Displays month, day, and year depending on the locale setting)
+        _datePicker.datePickerMode = UIDatePickerModeDate;
+        // 设置当前显示时间
+        [_datePicker setDate:[NSDate date] animated:YES];
+        // 设置显示最大时间（此处为当前时间）
+        [_datePicker setMaximumDate:[NSDate date]];
+        
+        //设置时间格式
+        
+        //监听DataPicker的滚动
+        [_datePicker addTarget:self action:@selector(dateChange:) forControlEvents:UIControlEventValueChanged];
+        
+        self.datePicker = _datePicker;
+        
+        
+    }
+    return _datePicker;
+}
 
 @end
